5f137674d1bcb538049532440f24a1db020dc0d5
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-10-03 09:09:15 -0500

    Map is now fullscreen on mobile

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,11 +1,33 @@
-var map = L.map('map').setView([50, 50], 18);
+// Initialize map
+var map = L.map('map').setView([0, 0], 10);
 
+// Add OpenStreetMap
 var layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
+// Sets map to current location, with tracking enabled
 map.locate({
     watch: true,
-    setView: true
+    setView: true,
+    maxZoom: 18,
+    enableHighAccuracy: true
 })
+
+// Locationfound while be fired multiple times while moving
+map.on('locationfound', (e) => {
+
+    var radius = e.accuracy
+
+    console.log('found');
+    L.marker(e.latlng).addTo(map)
+
+
+});
+
+map.on('locationerror', (e) => {
+
+    alert(e.message)
+
+});
