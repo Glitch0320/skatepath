44cf7b0c970a31525ffc1f2ae4c9c553e8d9a2ee
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-10-05 16:13:13 -0500

    May already have working path function... testing

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,6 +1,8 @@
 // Initialize map
 var map = L.map('map').setView([0, 0], 10);
 
+const path = []
+
 // Add OpenStreetMap
 var layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -23,12 +25,15 @@ map.on('locationfound', (e) => {
 
     console.log('found')
     console.log(e.latlng)
-    L.marker(e.latlng).addTo(map)
+    path.push(e.latlng)
+    // Clear polygon layer
+    console.log($('.leaflet-overlay-pane').children().eq(0).children().eq(0))
+    L.polygon(path).addTo(map)
     // Because locationfound will be fired almost constantly while moving, I could add each new point to a geoJSON object so that a line could be created out of all of the points.
     // At this point, I'm not sure how many points one object could keep track of without causing performance issues, since I've seen geoJSON objects representing lines as complex as state borders, I will assume for now that one skate will not be too much data
     // That being said, I could only add points if they fall outside of a certain distance from the initial point, so that they are added in a more incremental manner.
     // A feature I would like to allow for the user is the ability to edit their path on completion of a skate, because the tracking may not always be accurate. This is definitely possible with geoJSON.
-    
+
 
 });
 
