4ec55938da8e13801e140141dc57cf04d2b2c6ff
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-10-05 15:57:11 -0500

    Added some comments explaining my current goals

--- assets/js/code.js
+++ assets/js/code.js
@@ -15,31 +15,20 @@ map.locate({
     enableHighAccuracy: true
 })
 
-// Locationfound while be fired multiple times while moving
-const collection = []
-var first = true
+// Locationfound while be fired multiple times while moving because .locate is called with the watch option set to true
 
 // !locationfound: user can move and scale map
 // As soon as locationfound: the map is reset to current position due to setView and watch being true
 map.on('locationfound', (e) => {
 
-    const points = []
-
     console.log('found')
-    if (first) {
-        console.log(L)
-        const point = e.latlng
-        var radius = e.accuracy
-        L.marker(point).addTo(map).bindPopup('Your location is within ' + radius + ' meters from this marker.')
-        L.circle(point, radius).addTo(map)
-        first = false
-    }
-    const point = e.latlng
-    if (points.length < 3) {
-        points.push(point)
-    } else {
-        L.polyline(points, {color: 'blue'}).addTo(map)
-    }
+    console.log(e.latlng)
+    L.marker(e.latlng).addTo(map)
+    // Because locationfound will be fired almost constantly while moving, I could add each new point to a geoJSON object so that a line could be created out of all of the points.
+    // At this point, I'm not sure how many points one object could keep track of without causing performance issues, since I've seen geoJSON objects representing lines as complex as state borders, I will assume for now that one skate will not be too much data
+    // That being said, I could only add points if they fall outside of a certain distance from the initial point, so that they are added in a more incremental manner.
+    // A feature I would like to allow for the user is the ability to edit their path on completion of a skate, because the tracking may not always be accurate. This is definitely possible with geoJSON.
+    
 
 });
 
