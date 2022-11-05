eb679e00cdd139d33a3c427ed0f9255c028b36fa
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-11-05 16:26:33 -0500

    reworking logic, adding pseudocode

--- assets/js/code.js
+++ assets/js/code.js
@@ -13,8 +13,8 @@ let map = L.map('map').setView([0, 0], 10, mapOptions);
 
 // Tile Layer is a grid of images 256 by 256px
 let layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    "maxZoom": 18,
+    "attribution": '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 
 const pathOptions = {
@@ -24,19 +24,11 @@ const pathOptions = {
 // geoJSON object with one LineString
 const path = {
     "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        accuraccies: [],
-        timestamps: []
+    "properties": {
+        "accuraccies": [],
+        "timestamps": []
       },
-      "geometry": {
-        "type": "LineString",
-        coordinates: []
-      }
-    }
-  ]
+  "features": []
 };
 
 const lineOptions = {
@@ -44,58 +36,45 @@ const lineOptions = {
 }
 
 const gOptions = {
-    style: function (feature) {
-        return {
-            color: '#234099',
-            weight: 7.5
-        }
+    style: {
+    "color": '#234099',
+    "weight": 7.5,
+    "opacity": 0.85
     }
 }
 
 // Add geoJson to map
-var geoLayer = L.geoJSON().addTo(map)
+var geoLayer = L.geoJSON(path, gOptions).addTo(map)
 
 // Sets map to current location, with tracking enabled
 map.locate({
     watch: true,
+    setView: true,
     maxZoom: 18,
     enableHighAccuracy: true
 })
 
-// In this context, a point is a group of values to be added to the path object
+// path index is a direct count of every point added, while featureIndex is every two points(LineString)
 let pathIndex = 0
+let featureIndex = 0
 
 map.on('locationfound', (e) => {
 
-    console.log(`lat: ${e.latitude} lon: ${e.longitude}`)
-
-    // If first location 
-    if (pathIndex === 0) {
-        map.setView([e.latitude, e.longitude], 17)
-        // Add [lon, lat], timestamp, and accuracy to corresponding arrays in path{geoJSON} object
-        // Added Oslo Norway to check if map.distance returns true
-        path.features[0].geometry.coordinates = [[e.longitude, e.latitude]]
-        path.features[0].properties.timestamps = [e.timestamp]
-        path.features[0].properties.accuraccies = [e.accuracy]
-        console.log(path)
-        pathIndex++
-    // else if this location is > <lastlocationaccuracy> m from <lastlocation>
-    } else if ( map.distance(e.latlng, coordsToLatLng(path.features[0].geometry.coordinates[pathIndex - 1])) > path.features[0].properties.accuraccies[pathIndex - 1] ) {
-
-        // Add [lng, lat], timestamp, and accuracy to path
-        path.features[0].geometry.coordinates.push([e.longitude, e.latitude])
-        path.features[0].properties.timestamps.push(e.timestamp)
-        path.features[0].properties.accuraccies.push(e.accuracy)
-        console.log(path)
-        
-        if (pathIndex > 1) {
-            // redraw json
-            document.querySelector('.leaflet-overlay-pane').textContent = '';
-            L.geoJSON(path, gOptions).addTo(map)
+    const feature = {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: []
         }
-        
-        pathIndex++
     }
+
+    console.log(e)
+
+    // If new location is past a certain distance from previous one
+    
+        // if feature.coordinates.length < 2, add [e.lon, e.lat]
+
+        // Else, add feature to path.features and empty feature.coordinates, redraw
 
 });
 
